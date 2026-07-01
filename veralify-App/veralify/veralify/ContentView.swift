a96df6420d52a63fa978bcc798a26575c0a07082
Abdelrahman Abdelwahab <abdo.ayman.mail@gmail.com>
2026-07-01 15:23:58 +0200

    added the init project

---
+++ veralify-App/veralify/veralify/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  veralify
+//
+//  Created by Abdelrahman Abdelwahab on 01/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ veralify-App/veralify/veralifyTests/veralifyTests.swift
@@ -0,0 +1,19 @@
+//
+//  veralifyTests.swift
+//  veralifyTests
+//
+//  Created by Abdelrahman Abdelwahab on 01/07/2026.
+//
+
+import Testing
+@testable import veralify
+
+struct veralifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
