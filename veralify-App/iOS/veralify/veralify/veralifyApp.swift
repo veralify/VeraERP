fb26a3aa1947a2c5aca07357d3e3d84b1287ef19
Abdelrahman Abdelwahab <abdo.ayman.mail@gmail.com>
2026-07-01 15:27:45 +0200

    reAdded files

---
+++ veralify-App/iOS/veralify/veralify/veralifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  veralifyApp.swift
+//  veralify
+//
+//  Created by Abdelrahman Abdelwahab on 01/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct veralifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
